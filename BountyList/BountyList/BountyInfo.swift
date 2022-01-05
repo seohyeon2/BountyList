6c55bc3d86188231291f1a054a852eefb9a928e1
seohyeon2 <50102522+seohyeon2@users.noreply.github.com>
2022-01-05 15:57:24 +0900

    Add files via upload

---
+++ BountyList/BountyList/BountyInfo.swift
@@ -0,0 +1,22 @@
+//
+//  BountyInfo.swift
+//  BountyList
+//
+//  Created by 박서현 on 2021/07/04.
+//
+
+import UIKit
+
+struct BountyInfo {
+    let name: String
+    let bounty: Int
+    
+    var image: UIImage? {
+        return UIImage(named: "\(name).jpg")
+    }
+    
+    init(name: String, bounty: Int) {
+        self.name = name
+        self.bounty = bounty
+    }
+}
